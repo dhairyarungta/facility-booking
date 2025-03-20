d6d5614dec4097ab28a08f6ef39d10aa9c2e4472
Yashver1 <yashvershori@gmail.com>
2025-03-20 16:32:15 +0800

    (client) Feature : Add skeleton for client parsing

--- server/src/test.cpp
+++ server/src/test.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-
+#include<stdio.h>
 enum Day {
     Monday,
     Tuesday,
@@ -11,31 +11,41 @@ enum Day {
 };
 
 //56 bytes
-typedef struct RequestMessage {
-    char uid[8];
-    enum Day days[7];
-    char op;
-    char facilityName[4];
-    char startTime[4];
-    char endTime[4];
-    char offset[4];
-} RequestMessage;
+struct RequestMessage {
+    char uid[8]; //8 bytes
+    enum Day days[7]; //28 bytes
+    //3 byte padding
+    char op; //1 byte
+    char facilityName[4]; //4 bytes
+    char startTime[4]; // 4 bytes
+    char endTime[4]; // 4 bytes
+    char offset[4]; //4 bytes
+};
 
 //32 bytes
-typedef struct ReplyMessage {
-    char availabilities[8];
-    char uid[8];
-    unsigned int numAvail;
-    unsigned int errorCode;
-    unsigned int capacity;
-    enum Day day;
-} ReplyMessage;
+struct ReplyMessage {
+    char availabilities[8]; //8 bytes
+    char uid[8]; //8bytes
+    unsigned int numAvail; //4 bytes
+    unsigned int errorCode;  //4 bytes
+    unsigned int capacity; //4 bytes
+    enum Day day; //4bytes
+};
 
 int main(){
     std::cout << sizeof(Day) << std::endl;
 
     std::cout << sizeof(RequestMessage) << std::endl;
-
+    RequestMessage req = {
+        .uid = "USER123", 
+        .days = {Monday,Tuesday},
+        .op = 'B', 
+        .facilityName = {'G','Y','M'},
+        .startTime = {'0','9','0','0'},
+        .endTime = {'0','9','0','0'},
+    };
+    
+    __builtin_dump_struct(&req, &printf);
     std::cout << sizeof(ReplyMessage) << std::endl;
 
 
